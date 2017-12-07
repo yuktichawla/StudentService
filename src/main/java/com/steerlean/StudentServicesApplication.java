fcdb71b7004e636cf1801251724346400cf95888
Devesh Chanchlani <devesh.chanchlani@gmail.com>
2017-12-07 23:47:42 +0530

    initial commit

---
+++ src/main/java/com/steerlean/StudentServicesApplication.java
@@ -0,0 +1,11 @@
+package com.steerlean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentServicesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudentServicesApplication.class, args);
+    }
+}
