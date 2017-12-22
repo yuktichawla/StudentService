900834d5889d938985b5f7dfa06c0134474dd763
Devesh Chanchlani <devesh.chanchlani@gmail.com>
2017-12-22 12:19:48 +0530

    formatting jenkins groovy script

--- jenkinsPipeline.groovy
+++ jenkinsPipeline.groovy
@@ -1,45 +1,45 @@
 #!groovy
 
 node {
-   def mvnHome
+    def mvnHome
 
-   environment {
-       CREDENTIALS_ID = "44bb31cd-b259-4160-a31c-64403b8982d3"
-   }
+    environment {
+        CREDENTIALS_ID = "44bb31cd-b259-4160-a31c-64403b8982d3"
+    }
 
-   stage('Preparation') {
-      properties([
-         pipelineTriggers([[$class: 'GitHubPushTrigger'], 
-         pollSCM('')])
-      ])
-      checkout([
-         $class: 'GitSCM',
-         branches: [[name: '*/master']], 
-         doGenerateSubmoduleConfigurations: false, 
-         extensions: [], 
-         submoduleCfg: [], 
-         userRemoteConfigs: [[
-            credentialsId: '$CREDENTIALS_ID',
-            url: 'https://github.com/deveshchanchlani/StudentService.git'
-         ]]
-      ])         
-      mvnHome = tool 'maven3'
-   }
+    stage('Preparation') {
+        properties([
+                pipelineTriggers([[$class: 'GitHubPushTrigger'],
+                                  pollSCM('')])
+        ])
+        checkout([
+                $class                           : 'GitSCM',
+                branches                         : [[name: '*/master']],
+                doGenerateSubmoduleConfigurations: false,
+                extensions                       : [],
+                submoduleCfg                     : [],
+                userRemoteConfigs                : [[
+                                                            credentialsId: '$CREDENTIALS_ID',
+                                                            url          : 'https://github.com/deveshchanchlani/StudentService.git'
+                                                    ]]
+        ])
+        mvnHome = tool 'maven3'
+    }
 
-   stage('Test') {
-      // Run the maven build
-      if (isUnix()) {
-         sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean test"
-      } else {
-         bat(/"${mvnHome}\bin\mvn" -Dmaven.test.failure.ignore clean test/)
-      }
-   }
+    stage('Test') {
+        // Run the maven build
+        if (isUnix()) {
+            sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean test"
+        } else {
+            bat(/"${mvnHome}\bin\mvn" -Dmaven.test.failure.ignore clean test/)
+        }
+    }
 
-   stage('IntegrationTest') {
-      if(isUnix()) {
-         sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean verify -P integration-test"
-      } else {
-         bat(/"${mvnHome}\bin\mvn" -Dmaven.test.failure.ignore clean verify -P integration-test/)
-      }
-   }
+    stage('IntegrationTest') {
+        if (isUnix()) {
+            sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean verify -P integration-test"
+        } else {
+            bat(/"${mvnHome}\bin\mvn" -Dmaven.test.failure.ignore clean verify -P integration-test/)
+        }
+    }
 }
