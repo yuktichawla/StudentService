488583975c19542ece5c38a64c54a75c0ce6d04e
Devesh Chanchlani <devesh.chanchlani@gmail.com>
2017-12-22 16:06:37 +0530

    checking in final version of jenkins file

--- jenkinsPipeline.groovy
+++ jenkinsPipeline.groovy
@@ -4,31 +4,18 @@ node {
     def mvnHome
 
     environment {
-        CREDENTIALS_ID = "44bb31cd-b259-4160-a31c-64403b8982d3"
-        AUTH_TOKEN = "git_push_hook"
+        GIT_CREDENTIALS_ID = "44bb31cd-b259-4160-a31c-64403b8982d3"
     }
 
     stage('Preparation') {
-        properties([
-                pipelineTriggers([[$class: 'GitHubPushTrigger'],
-                                  pollSCM('')])
-        ])
-        checkout([
-                $class                           : 'GitSCM',
-                branches                         : [[name: '*/master']],
-                doGenerateSubmoduleConfigurations: false,
-                extensions                       : [],
-                submoduleCfg                     : [],
-                userRemoteConfigs                : [[
-                                                            credentialsId: '$CREDENTIALS_ID',
-                                                            url          : 'https://github.com/deveshchanchlani/StudentService.git'
-                                                    ]]
-        ])
+        git changelog: false,
+            credentialsId: '$GIT_CREDENTIALS_ID',
+            poll: false,
+            url: 'https://github.com/deveshchanchlani/StudentService.git'
         mvnHome = tool 'maven3'
     }
 
     stage('Test') {
-        // Run the maven build
         if (isUnix()) {
             sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean test"
         } else {
